565560d8b28ea046c3c2aae7a295b79dce47a1ea
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2024-01-04 23:52:58 +0530

    arrays

--- builtInMethods.js
+++ builtInMethods.js
@@ -1,4 +1,5 @@
-// BIG-O OF OBJECTS ( KEY-VALUE PAIRS) uses hashmaps
+//========= BIG-O OF OBJECTS ( KEY-VALUE PAIRS) uses hashmaps=========
+
 // INSERTION - O(1)
 // REMOVAL - O(1)
 // SEARCHING - O(N)
@@ -14,7 +15,18 @@ let instructor = {
 
 // OBEJECT.keys() - O(n)
 console.log(Object.keys(instructor));
+// OBEJECT.values() - O(n)
+console.log(Object.values(instructor));
 // OBEJECT.entries() - O(n) -though more operations then keys method
 console.log(Object.entries(instructor));
 // OBEJECT.hasOwnProperty() - O(1)
 console.log(Object.hasOwnProperty("nam"));
+
+//========= BIG-O OF ARRAYS ( ordered DS) =========
+
+// insertion BIG-O depends on where we want to insert the new element
+// removal BIG-O depends on where we want to insert the new element
+// searching - O(N)
+// access - O(1)
+
+let names = ["ram", "shyam", "ghyansam"];
