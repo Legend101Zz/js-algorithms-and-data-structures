564b983e9dbbc5a17af579550e9c4be73ab5ebb7
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2024-01-04 23:44:45 +0530

    object methods

--- builtInMethods.js
+++ builtInMethods.js
@@ -3,3 +3,18 @@
 // REMOVAL - O(1)
 // SEARCHING - O(N)
 // ACCESS - O(1)
+
+//METHODS
+
+let instructor = {
+  name: "Mrigesh",
+  isHealthy: true,
+  classes: [2, 5, 6],
+};
+
+// OBEJECT.keys() - O(n)
+console.log(Object.keys(instructor));
+// OBEJECT.entries() - O(n) -though more operations then keys method
+console.log(Object.entries(instructor));
+// OBEJECT.hasOwnProperty() - O(1)
+console.log(Object.hasOwnProperty("nam"));
